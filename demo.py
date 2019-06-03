f94942575522933f648c558630c39a20006a44c4
oyiadin <oyiadin@gmail.com>
2019-06-03 20:41:03 +0800

    demo update

--- demo.py
+++ demo.py
@@ -7,29 +7,10 @@ import shutil
 
 import cv2
 import numpy as np
-import tensorflow as tf
-# import scipy.io as sio
 from scipy.misc import imsave
 from torchver.tvnet import TVNet
-from torch.utils.data import DataLoader
 import torch
-from torch.autograd import Variable
 
-# flags = tf.app.flags
-# flags.DEFINE_integer("scale", 5, " TVNet scale [3]")
-# flags.DEFINE_integer("warp", 5, " TVNet warp [1]")
-# flags.DEFINE_integer("iteration", 50, " TVNet iteration [10]")
-# flags.DEFINE_string("gpu", '0', " gpu to use [0]")
-# FLAGS = flags.FLAGS
-#
-# scale = FLAGS.scale
-# warp = FLAGS.warp
-# iteration = FLAGS.iteration
-# if int(FLAGS.gpu > -1):
-#     os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
-#
-# print('TVNet Params:\n scale: %d\n warp: %d\n iteration: %d\nUsing gpu: %s' \
-#       % (scale, warp, iteration, FLAGS.gpu))
 
 scale = 1
 warp = 1
@@ -38,6 +19,7 @@ iteration = 1
 
 lines = open('/ds/hmdb_frames/mini_test.txt').readlines()
 filename, label = random.choice(lines).split()
+print(filename, label)
 basedir = filename[:-len('.avi')]
 path = os.path.join('/ds/hmdb_frames/', label, basedir)
 frames = [f for f in os.listdir(path) \
@@ -54,13 +36,17 @@ N = images.shape[0]
 _ = cv2.imread(os.path.join(path, '00001.jpg'))
 h, w, c = _.shape
 
+t = time.time()
+
 # model construction
 x1 = torch.Tensor(images[:N-1, ...], device=torch.device('cpu'))
 x2 = torch.Tensor(images[1:, ...], device=torch.device('cpu'))
 tvnet = TVNet()
-u1, u2, rho = tvnet.tvnet_flow(x1,x2,max_scales=scale,
+u1, u2, rho = tvnet.tvnet_flow(x1, x2,max_scales=scale,
                      warps=warp,
                      max_iterations=iteration)
+
+print(time.time()-t)
 
 # run model
 u1_np, u2_np = u1.detach().numpy(), u2.detach().numpy()
